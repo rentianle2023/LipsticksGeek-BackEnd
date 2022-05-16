929beac6315795f73ba926ac2e53f6f88d19ad2c
tyler <515322780@qq.com>
2022-05-16 20:37:48 +0800

    update CI/CD

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11
 EXPOSE 8080
 ADD target/springboot-lipstick.jar springboot-lipstick.jar
-ENTRYPOINT ["java","-Dspring.profiles.active=dev","-jar","springboot-lipstick.jar"]
+ENTRYPOINT ["java","-Dspring.profiles.active=prod","-jar","springboot-lipstick.jar"]
--- src/main/java/fun/tianlefirstweb/www/chat/WebSocketConfig.java
+++ src/main/java/fun/tianlefirstweb/www/chat/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3000","http://192.168.101.19:3000")
+                .setAllowedOriginPatterns("http://localhost:3000","http://192.168.101.19:3000","http://www.tianlefirstweb.fun")
                 .withSockJS();
     }
 }
