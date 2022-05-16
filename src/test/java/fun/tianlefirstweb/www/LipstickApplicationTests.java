b5e713ec0954c4d36eda7d8e334b66d59668a66a
tyler <515322780@qq.com>
2022-05-16 16:06:23 +0800

    fix test

--- src/test/java/fun/tianlefirstweb/www/LipstickApplicationTests.java
+++ src/test/java/fun/tianlefirstweb/www/LipstickApplicationTests.java
@@ -2,8 +2,10 @@ package fun.tianlefirstweb.www;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class LipstickApplicationTests {
 
     @Test
