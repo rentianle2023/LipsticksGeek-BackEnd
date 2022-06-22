f52c3cda972f742db7b8be804dfce588422d3615
tyler <515322780@qq.com>
2022-06-22 16:59:45 +0800

    clear test

--- src/test/java/fun/tianlefirstweb/www/LipstickApplicationTests.java
+++ src/test/java/fun/tianlefirstweb/www/LipstickApplicationTests.java
@@ -4,12 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("dev")
 class LipstickApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
